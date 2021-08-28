b964e19e60557b6f02419cebe769b87a4587c474
YH <kevin95392004@gmail.com>
2021-08-28 17:36:01 +0900

    컨트롤러 예외처리 핸들러 구현

---
+++ source/src/main/java/io/torder/exam/controller/common/GeneralExceptionHandler.java
@@ -0,0 +1,59 @@
+package io.torder.exam.controller.common;
+
+import org.hibernate.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GeneralExceptionHandler {
+
+    /**
+     * HttpHeaders와 ApiResponse를 같이 내려주기 위하여 ResponseEntity를 만드는 메서드
+     */
+    private ResponseEntity<ApiResponse<?>> response(HttpStatus status, Throwable throwable) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/json");
+        return new ResponseEntity<>(ApiResponse.ERROR(status, throwable), headers, status);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(Exception e) {
+        return response(HttpStatus.NOT_FOUND, e);
+    }
+
+    @ExceptionHandler({
+            IllegalStateException.class, IllegalArgumentException.class,
+            TypeMismatchException.class, HttpMessageNotReadableException.class,
+            MissingServletRequestParameterException.class, MultipartException.class,
+    })
+    public ResponseEntity<?> handleBadRequestException(Exception e) {
+        return response(HttpStatus.BAD_REQUEST, e);
+    }
+
+    @ExceptionHandler(HttpMediaTypeException.class)
+    public ResponseEntity<?> handleHttpMediaTypeException(Exception e) {
+        return response(HttpStatus.UNSUPPORTED_MEDIA_TYPE, e);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> handleMethodNotAllowedException(Exception e) {
+        return response(HttpStatus.METHOD_NOT_ALLOWED, e);
+    }
+
+    /**
+     * 예측하지 못한 이외의 Exception Handling
+     */
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity<?> handleException(Exception e) {
+        return response(HttpStatus.INTERNAL_SERVER_ERROR, e);
+    }
+}
